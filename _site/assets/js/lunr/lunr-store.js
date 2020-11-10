26a941f3db8f3c8329aecda1809acb8ddb3dbd92
junesim <junesimtom@gmail.com>
2020-11-10 08:46:43 +0000

    Add Third post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -22,4 +22,22 @@ var store = [{
         "tags": [],
         "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/type-of-network/",
         "teaser": null
+      },{
+        "title": "관계대수와 관계해석",
+        "excerpt":"기능이 같지만 이름이 다른 관계대수와 관계해석에 대해 알아보자. 관계대수   관계대수란 R-DB(Relational Database)에서 원하는 정보와 그 정보를 검색하기 위해 어떻게 검색할 것인지를 기술하는 절차적인 언어이다. 순수관계연산자, 일반 집합연산자가 해당된다. 릴레이션을 처리하기 위한 연산의 집합으로 피연산자와 결과가 모두 릴례이션이다. 관계해석   관계해석이란 비절차적인 언어로원하는 정보가 무엇이라는 것만 정의하고 이를 통해 검색하는 것을 의미한다....","categories": ["데이터베이스"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4/Database/",
+        "teaser": null
+      },{
+        "title": "Server",
+        "excerpt":"  서버란 무엇인가. 사용자들에게 자원을 제공해 주는 것. 웹 서버   기본 APM(Apache(웹 서버), PhP(프로그래밍 언어), MySql(데이터베이스,요즘엔 MariaDB) 이렇게 해서 APM이라고 불린다. 기본이지만 요즘에는 장고(Django) 나 다른 방법으로 구축할 수 있습니다. 요즘엔 Apache 대신 Tomcat을 사용합니다.   보통 웹 서버의 경우 리눅스를 이용해서 구축을 많이 하는데 리눅스의 대표적인 특징에는 쉘과 커널이 있다. 쉘...","categories": ["ICT"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/ict/Server/",
+        "teaser": null
+      },{
+        "title": "WEB Server와 WAS의 차이",
+        "excerpt":"Web Server와 WAS에 대해 알아보자. Web Server 웹 서비스를 할 수 있는 환경을 구축하기 위해 사용하는 소프트웨어를 의미한다. HTTP를 통해 클라이언트가 요청하는 HTML 문서 및 오브젝트를 전송해주는 서버를 의미한다. 정적 콘텐츠를 제공해 주기 때문에 항상 같은 결과를 받을 수 있다. ex) Apache WAS(Web Application Server) 동적인 컨텐츠를 제공하기 위해 만들어진...","categories": ["ICT"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/ict/WEB-WAS/",
+        "teaser": null
       }]
