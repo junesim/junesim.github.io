9e5f1d3050538e70bcaa0fa8340ab7e4ea0bb8f7
junesim <junesimtom@gmail.com>
2022-04-09 17:32:45 +0900

    update post 2021.04.09

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,90 +2,90 @@ var store = [{
         "title": "How to make a git blog",
         "excerpt":"  깃 블로그를 만들어보자.   깃 블로그를 만드는 방법은 매우 쉬워보이고 실제로 쉽기도 하다.   모두가 가지고 있는 블로그를 생성해보고 원하는 것을 공유해보자!   구름IDE와 Jekyll를 이용해 블로그를 생성해보겠다. 일단 Github에서 레포지토리 이름을 username.github.io로 생성 해주자. 꼭 위 방법대로 생성할 필요는 없다. 자신이 원하는 이름으로 레포지터리를 생성한 뒤 설정에서 깃허브 페이지 설정을 지정해 페이지를...","categories": ["깃 블로그 만들기"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EA%B9%83%20%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/How-to-make-a-git-blog/",
+        "url": "http://localhost:4000/%EA%B9%83%20%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/How-to-make-a-git-blog/",
         "teaser": null
       },{
         "title": "Spoofing",
         "excerpt":"  Kali와 CentOS에서 ARP Spoofing과 DNS Spoofing을 실습해보자. IP Spoofing   IP Spoofing이란 자신의 ip를 변조해 자신이 만든 패킷을 다른 컴퓨터가 만든 패킷처럼 만드는 것이다. Fragroute   포트 포워딩을 해주는 도구.   ARP Spoofing을 하게 되면 자신에게 패킷이 오게 되는데, 이후 다시 피해자가 지정한 곳에 피해자의 패킷을 보내주기 위해 사용한다. 만약 이를 사용하지 않으면...","categories": ["정보보안"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/Spoofing/",
+        "url": "http://localhost:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/Spoofing/",
         "teaser": null
       },{
         "title": "DoS 공격 - LAND ATTACK, DDoS",
         "excerpt":"  hping3를 이용해 DoS 공격을 시도해보자. DoS   Denial of Service의 약자로 피해자에게 과도한 트래픽을 줘서 정상적인 처리를 하지 못하도록 하는 것이다. 다양한 종류가 있으나 여기선 LAND Attack과 DDoS에 대해 알아보자. LAND Attack   Land Attack이란 source ip(송신자의 ip)와 destination ip(수신자의 ip)를 같게 만들어 공격하는 방법이다. 자신의 IP를 변조해 공격을 하기 떄문에 이는...","categories": ["정보보안"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/Network-Attack-With-HPING3/",
+        "url": "http://localhost:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/Network-Attack-With-HPING3/",
         "teaser": null
       },{
         "title": "네트워크 통신 방식의 종류",
         "excerpt":"  통신방식들에 대해 알아보자.   통신 방식을 나누는 방법엔 다양한 방법이 있다. 크게 연결형(Connection)과 비연결형(Connectionless)으로 나눌 수 있다. 연결형(Connection)   데이터 송신을 시작하기 전에 호스트 간의 회선을 논리적으로 연결한 후 통신을 시작한다. 연결의 확립과 끊기 과정이 필요하지만 신뢰성이 높다. 데이터의 순서가 중요할 경우 적합하다. 전화를 생각하면 이해가 편할듯 하다. 대표적인 프로토콜로는 TCP가 해당된다....","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/type-of-network/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/type-of-network/",
         "teaser": null
       },{
         "title": "관계대수와 관계해석",
         "excerpt":"기능이 같지만 이름이 다른 관계대수와 관계해석에 대해 알아보자. 관계대수   관계대수란 R-DB(Relational Database)에서 원하는 정보와 그 정보를 검색하기 위해 어떻게 검색할 것인지를 기술하는 절차적인 언어이다. 순수관계연산자, 일반 집합연산자가 해당된다. 릴레이션을 처리하기 위한 연산의 집합으로 피연산자와 결과가 모두 릴례이션이다. 관계해석   관계해석이란 비절차적인 언어로원하는 정보가 무엇이라는 것만 정의하고 이를 통해 검색하는 것을 의미한다....","categories": ["데이터베이스"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4/Database/",
-        "teaser": null
-      },{
-        "title": "Server",
-        "excerpt":"  서버란 무엇인가. 사용자들에게 자원을 제공해 주는 것. 웹 서버   기본 APM(Apache(웹 서버), PhP(프로그래밍 언어), MySql(데이터베이스,요즘엔 MariaDB) 이렇게 해서 APM이라고 불린다. 기본이지만 요즘에는 장고(Django) 나 다른 방법으로 구축할 수 있습니다. 요즘엔 Apache 대신 Tomcat을 사용합니다.   보통 웹 서버의 경우 리눅스를 이용해서 구축을 많이 하는데 리눅스의 대표적인 특징에는 쉘과 커널이 있다. 쉘...","categories": ["ICT"],
-        "tags": [],
-        "url": "http://0.0.0.0:4000/ict/Server/",
+        "url": "http://localhost:4000/%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4/Database/",
         "teaser": null
       },{
         "title": "WEB Server와 WAS의 차이",
         "excerpt":"Web Server와 WAS에 대해 알아보자. Web Server 웹 서비스를 할 수 있는 환경을 구축하기 위해 사용하는 소프트웨어를 의미한다. HTTP를 통해 클라이언트가 요청하는 HTML 문서 및 오브젝트를 전송해주는 서버를 의미한다. 정적 콘텐츠를 제공해 주기 때문에 항상 같은 결과를 받을 수 있다. ex) Apache WAS(Web Application Server) 동적인 컨텐츠를 제공하기 위해 만들어진...","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/WEB-WAS/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/WEB-WAS/",
         "teaser": null
       },{
         "title": "DNS",
         "excerpt":"DNS란 무엇인가 DNS 서버 Domain Name Server의 약자. 모든 통신은 IP로 통신이 되는데 이를 사람이 기억하긴 힘드니 IP를 도메인으로 바꿔주는 역할을 한다. 기존에는 hosts 파일(호스트 파일)을 이용해 네트워크에 접속했는데 네트워크 상의 컴퓨터의 증가로 이를 일일히 개인이 기억하는 것은 비효율적. 따라서 이를 전문적으로 해주는 DNS 서버를 생성해서 이용하게 된다. ex) PowerDNS,ndjbdns,knot,unbound,bind...","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/DNS/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/DNS/",
         "teaser": null
       },{
         "title": "DHCP",
         "excerpt":"DHCP란 무엇인가 DHCP Dynamic Host Configuration Protocol의 약자 DHCP 서버가 하는 역할은 자신의 네트워크 안에 있는 클라이언트 컴퓨터가 부팅될 떄 자동으로 네트워크 설정(IP,서브넷 마스크, 게이트웨이 주소, DNS 서버 주소)을 할당해 주는 것이다. 서버의 동작 방식 동적 할당 : 네트워크 관리자가 일정 범위의 IP 주소를 지정하고 클라이언트의 요청에 따라 IP를 할당해...","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/DHCP/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/DHCP/",
         "teaser": null
       },{
         "title": "메일 프로토콜과 구성요소",
         "excerpt":"메일 프로토콜과 구성요소에 대해 알아보자. 메일 프로토콜 SMTP : Simple Mail Transfer Protocol 클라이언트에서 메일을 보내거나 메일 서버끼리 메일을 주고 받을 떄 사용하는 프로토콜 POP3, IMAP3 : 메일 서버에 있는 메일을 클라이언트로 가져오는 프로토콜 보낼 때는 SMTP 받을 때는 POP3, IMAP3 차이점 POP3의 경우 클라이언트가 서버의 메일을 가져오게 되면 서버의...","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/Mail-Protocol/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/Mail-Protocol/",
         "teaser": null
       },{
         "title": "프로토콜에 따른 패킷의 헤더",
         "excerpt":"프로토콜에 따른 패킷의 헤더들에 대해 알아보자.   UDP 패킷 헤더   UDP 패킷 헤더는 64비트로 16비트의 4가지 필드로 구성되어 있다.      Source Port Number: 송신 포트 번호   Destination Port Number: 수신 포트 번호   UDP Length: 패킷의 길이   UDP Checksum: 패킷 전체의 Checksum, 이 필드가 0일 경우 수신측은 체크섬을 계산하지 않는다.  ","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/Network-UDP-Header/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/Network-UDP-Header/",
         "teaser": null
       },{
         "title": "파이썬",
         "excerpt":"파이썬에 대해 알아보자. 프로그래밍 언어엔 컴파일러 언어와 인터프리터 언어가 있다. 컴파일러 언어 보통 우리가 많이 사용했던 JAVA, C++, C 등은 컴파일러 언어라고한다. 컴파일러가 컴파일 과정에서 소스 코드를 기계언어로 바꾸고 이를 컴퓨터에서 동작시킨다. 반응성이 빠르지만 빌드 과정에서 시간이 소요되 작은 수정을 하더라도 빌드 시간이 오래 걸린다. 인터프리터 언어 인터프리터 언어의 경우...","categories": ["파이썬"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%ED%8C%8C%EC%9D%B4%EC%8D%AC/Python/",
+        "url": "http://localhost:4000/%ED%8C%8C%EC%9D%B4%EC%8D%AC/Python/",
         "teaser": null
       },{
         "title": "IP Class들과 서브넷",
         "excerpt":"IP 클래스들에 대해 알아보자. IP의 경우 5개의 클래스로 이루어져있다. 그리고 클래스 별로 서브넷 마스크가 달라 이에 대한 숙지가 필요하다. 공인 IP와 사설 IP 공인 IP란 전 세계에서 유일한 IP 주소로 ISP가 제공하는 IP 주소이다. 외부에 공개된 주소로 인터넷에 연결된 다른 PC로부터의 접근이 가능하다. 즉 외부 네트워크에 연결하기 위해 필요한 IP로...","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/IP-Class/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/IP-Class/",
         "teaser": null
       },{
         "title": "GNS3 Firewall 실습",
         "excerpt":"GNS3에서 방화벽 Transparent 모드를 실습해 보자. Transparent 모드 방화벽을 2계층에서 동작 시킨다. 외부에서 봤을 때 홉수가 달라지지 않아 존재를 파악하기 힘들다. -&gt; 보안적으로 뛰어나다?! 기존 장비들의 IP 주소를 변경시킬 필요가 없다. 동일한 서브넷 상에서 이동하는 패킷을 검사하고 필터링할 수 있다. 2계층 트래픽 검사하고 원하지 않는 트래픽 필터링 가능 관리용 IP가...","categories": ["네트워크"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/Firewall-in-gns3/",
+        "url": "http://localhost:4000/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/Firewall-in-gns3/",
         "teaser": null
       },{
         "title": "SQL Injection",
         "excerpt":"SQL Injection에 대해 알아보자. SQL Injection이란 공격자가 SQL 구문을 이용해 서버의 정보를 탈취하거나 인증을 우회하는 공격 기법이다. SQL Injection에 많이 사용되는 함수들을 알아보자. 밑에 있는 함수들은 주로 MySQL에서 사용되는 함수들이다. USER 사용자와 호스트를 출력해주는 함수이다. 함수의 형태는 user()이고, select user()로 쿼리를 이용해 현재 사용자와 호스트를 출력할 수 있다. VERSION 현재...","categories": ["정보보안"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/SQL-Injection/",
+        "url": "http://localhost:4000/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/SQL-Injection/",
+        "teaser": null
+      },{
+        "title": "Server",
+        "excerpt":"서버란 무엇인가. 사용자들에게 서비스를 제공해 주는 것. 사용자(Client)들의 요구에 따라서 필요한 서비스를 제공해주는 장비다. 어원은 특정 역할에 특화 된 것을 의미한다고 한다. 용도에 따라서 구조가 나눠지는데 크게 클라이언트-서버 구조와 3-tier 구조로 나눌수 있다. 클라이언트-서버 구조 사용자(Client)가 특정 프로그램(Client Program)을 설치하고 이를 이용해 서버에 접속 해 서버가 제공하는 서비스를 이용한다. 해당...","categories": ["ICT"],
+        "tags": [],
+        "url": "http://localhost:4000/ict/Server/",
         "teaser": null
       }]
